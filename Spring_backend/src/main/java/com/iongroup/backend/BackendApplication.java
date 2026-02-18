f5cfdbe12e627396941aa51744cb71ad4a5c9d1d
Aryan Laxaman <aryan.laxman@iongroup.com>
2026-02-19 00:53:41 +0530

    just a working image of spring from potential branch

--- Spring_backend/src/main/java/com/iongroup/backend/BackendApplication.java
+++ Spring_backend/src/main/java/com/iongroup/backend/BackendApplication.java
@@ -12,16 +12,16 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 
-		System.out.println("hehe\n");
+		// System.out.println("hehe\n");
 
-		System.out.println(
-				RegistryConfiguration.class
-						.getClassLoader()
-						.getResource("com/iongroup/library/adapter/flowable")
-		);
+		// System.out.println(
+		// 		RegistryConfiguration.class
+		// 				.getClassLoader()
+		// 				.getResource("com/iongroup/library/adapter/flowable")
+		// );
 		
-		DelegationService myService = new DelegationService();
-		System.out.println(myService.getDelegationCount());
+		//DelegationService myService = new DelegationService();
+		//System.out.println(myService.getDelegationCount());
 	}
 
 }
